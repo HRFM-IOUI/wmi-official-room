9bc531426d9da6292a8c9b97d98c1def8b000919
i know <ik39.10vevic@gmail.com>
2025-06-21 17:09:21 +0900

    wanfixnyanfix

--- src/app/nyan/page.tsx
+++ src/app/nyan/page.tsx
@@ -1,4 +1,6 @@
+// src/app/wan/page.tsx
 import NyanFeed from "@/components/NyanFeed";
+
 export default function Page() {
   return <NyanFeed />;
 }
---
+++ src/app/wan/page.tsx
@@ -0,0 +1,6 @@
+// src/app/wan/page.tsx
+import WanFeed from "@/components/WanFeed";
+
+export default function Page() {
+  return <WanFeed />;
+}
